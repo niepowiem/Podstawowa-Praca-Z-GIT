5f8e7478dc9ff1f2516d1cc73c8e573b9900cacc
Bartosz Zieliński <niepodamcilamo@gmail.com>
2025-05-06 15:38:10 +0200

    add --logs functionality for 100 files

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,11 @@
 if [ "$1" == "--date" ]; then
     date +"%Y-%m-%d"
 fi
+
+if [ "$1" == "--logs" ] && [ -z "$2" ]; then
+    for i in {1..100}; do
+        echo "Nazwa pliku: log${i}.txt" > log${i}.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log${i}.txt
+        echo "Data: $(date +"%Y-%m-%d")" >> log${i}.txt
+    done
+fi
