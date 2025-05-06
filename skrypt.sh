9f98afe1a23ea3e6131d618e9b330da33f27fcf9
Bartosz Zieliński <niepodamcilamo@gmail.com>
2025-05-06 15:40:40 +0200

    add --logs functionality with count argument

--- skrypt.sh
+++ skrypt.sh
@@ -4,8 +4,9 @@ if [ "$1" == "--date" ]; then
     date +"%Y-%m-%d"
 fi
 
-if [ "$1" == "--logs" ] && [ -z "$2" ]; then
-    for i in {1..100}; do
+if [ "$1" == "--logs" ] && [ -n "$2" ]; then
+    liczba_plikow=$2
+    for i in $(seq 1 $liczba_plikow); do
         echo "Nazwa pliku: log${i}.txt" > log${i}.txt
         echo "Nazwa skryptu: skrypt.sh" >> log${i}.txt
         echo "Data: $(date +"%Y-%m-%d")" >> log${i}.txt
